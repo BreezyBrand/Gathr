03017e4e6596e0c82290b19634aebee33bfc6627
Brandon Sorensen <brandonjamessorensen@gmail.com>
2025-11-12 15:50:42 -0500

    Added card type search

---
+++ wwwroot/SQL/Get Types By Card Type.sql
@@ -0,0 +1,28 @@
+﻿SELECT DISTINCT
+	id,
+	type_line,
+	'Whole Card' as [Card Type]
+FROM 
+	Cards c
+WHERE CHARINDEX('Token',type_line) = 0
+AND	  CHARINDEX('//', type_line) < 1
+UNION ALL SELECT 
+				id,
+				CASE WHEN CHARINDEX('//', type_line) > 2 THEN SUBSTRING(type_line, 1, CHARINDEX('//', type_line) - 1) 
+				ELSE type_line
+				END as type_line,
+				'Split Card A'
+				FROM Cards			
+WHERE CHARINDEX('Token',type_line) = 0
+AND	  CHARINDEX('//', type_line) > 1
+UNION ALL SELECT 
+				id,
+				CASE WHEN CHARINDEX('//', type_line) > 2 THEN SUBSTRING(type_line, LEN(SUBSTRING(type_line, 1, CHARINDEX('//', type_line) - 1) )+5, 1000) 
+				ELSE type_line
+				END as Back,
+				'Split Card B'				
+				FROM Cards			
+WHERE CHARINDEX('Token',type_line) = 0
+AND	  CHARINDEX('//', type_line) > 1
+ORDER BY id
+	
